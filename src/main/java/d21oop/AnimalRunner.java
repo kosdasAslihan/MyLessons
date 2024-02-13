9bcc66ebe2bb0bbac7a2b160e4936f55cae2d2f3
kosdasAslıhan <kosdasaslihan@gmail.com>
2024-02-13 23:26:39 +0300

    day21lesson

---
+++ src/main/java/d21oop/AnimalRunner.java
@@ -0,0 +1,33 @@
+package d21oop;
+
+public class AnimalRunner {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.bark(); //Dogs bark...
+        d.feedWithMilk(); //Mammals feed their babies with milk
+        d.drink(); //Animals drink...
+        //d.eat(); //Animals eat... override öncesi
+        d.eat(); //Dogs eat... override yaptiktan sonra
+
+        //Bird - object
+        Bird b = new Bird();
+        b.tweet(); //Birds tweet...
+        b.eat(); //Animals eat...
+
+        Cat c = new Cat();
+        c.feedWithMilk(); //Cats feed their babies with milk
+
+        //a) Java ‘Multiple Inheritance’ i desteklemez. ‘Single Inheritance’ i destekler.
+
+        //b) Parent ve birden fazla child ile olusturulan inheritance yapilanmasina
+        //  ‘Hierarchical Inheritance’ denir ve Java bunu kabul eder.
+
+        /*c) Ama coklu parent'lardan yararlanamamak iyi olmadi. Ozelliklerini inherit etmek istedigimiz
+        class'lar birden fazla ise (parent'lar) o durumda Java bize, Apartman tarzi yapilanma olusturmustur.
+        ( 'Multi Level Inheritance') Java bunu kabul eder.*/
+
+        // Java’da parent’tan child’a olan iliskilere HAS-A Relationship denir(yukaridan asagiya)
+        // Java’da child’tan parent’a olan iliskilere IS-A Relationship denir (asagidan yukariya)
+
+    }
+}
--- src/main/java/d21oop/Vehicle.java
+++ src/main/java/d21oop/Vehicle.java
@@ -1,4 +1,4 @@
-package d20instanceblock_oop;
+package d21oop;
 
 public class Vehicle {
 
@@ -16,11 +16,11 @@ public class Vehicle {
      */
 
     //Constructor olusturalim
-    public Vehicle(){ //Parametresizler: 1-default 2-no arguman
-        super(); //3
+    public Vehicle(){ //4 //E
+        this("Honda",2024,"Elektrikli");
         System.out.println("Honda,2023,Benzin");
     }
-    public Vehicle(String marka, int yil, String motor){
+    public Vehicle(String marka, int yil, String motor){ //F
         super();
         System.out.println("Honda,2024,Elektrikli");
 
